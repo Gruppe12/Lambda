e630d53cd760802d5a39b97e3b060d55e7833b13
Daniel Pihlstrøm Karlsen <danielpk@hiof.no>
2025-10-16 15:47:08 +0200

    Metoder for utskriving av SQL-spørringer.

--- src/main/java/no/lambda/port/ReiseKlarPort.java
+++ src/main/java/no/lambda/port/ReiseKlarPort.java
@@ -6,4 +6,10 @@ public interface ReiseKlarPort {
 
     void createFavoriteRoute(Rute rute) throws EnTurException;
     Rute getFavoriteRoute(int favorittruteId) throws EnTurException;
+    void createUserSQLQuery(String fornavn, String etternavn) throws EnTurException;
+    void createFavoriteRouteSQLQuery(int brukerId, double fromLongitude, double fromLatitude, double ToLongitude, double ToLatitude, int ToPlaceId) throws EnTurException;
+    void getToAndFromBasedOnFavoriteRouteIDAndUserIDSQLQuery(int favorittruteId, int brukerId) throws EnTurException;
+    void getFavoriteRoutesFromUserBasedOnIdSQLQuery(int brukerId) throws EnTurException;
 }
+
+
