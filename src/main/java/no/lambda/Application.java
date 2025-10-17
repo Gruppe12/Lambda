781e0be2ae535249c147b8d798a748065e9a3954
churchred <churchred@outlook.com>
2025-10-17 11:03:24 +0200

    Update Application.java

--- src/main/java/no/lambda/Application.java
+++ src/main/java/no/lambda/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
     public static void main(String[] args) {
 
+        // Makes the app object based on the HTML/CSS/JS in the public folder
+        // We connect it a port, and start hosting it.
         Javalin app = Javalin.create(config -> {
             config.staticFiles.add("/public", Location.CLASSPATH);
         }).start(8080);
