ce118ff15c4c0d84bd5deed8a38652c0abcade3b
Daniel Pihlstrøm Karlsen <danielpk@hiof.no>
2025-10-29 10:37:45 +0100

    Fikset databasetilkobling.

--- src/main/java/no/lambda/Application.java
+++ src/main/java/no/lambda/Application.java
@@ -3,12 +3,13 @@ import no.lambda.Services.EnturService;
 import no.lambda.Storage.adapter.ReiseKlarAdapter;
 import no.lambda.Storage.database.MySQLDatabase;
 import no.lambda.client.entur.EnturGraphQLClient;
+import no.lambda.model.Rute;
 import no.lambda.port.ReiseKlarPort;
-
 import java.sql.Connection;
 
+
 public class Application {
-    private final static String URL = "jdbc:mysql://158.39.162.26:3306/Lambda";
+    private final static String URL = "jdbc:mysql://itstud.hiof.no:3306/se25_G12";
     private final static String USERNAME = "gruppe12";
     private final static String PASSWORD = "Summer31";
 
@@ -22,8 +23,13 @@ public class Application {
         ReiseKlarPort reiseKlar = new ReiseKlarAdapter(dbConnection);
 
         EnturGraphQLClient client = new EnturGraphQLClient();
+
         // Oppretter EnturService
-        EnturService enturService = new EnturService(client, reiseKlar);
+        // EnturService enturService = new EnturService(client, reiseKlar);
+
+        // Demonstrasjon av opprettelse av en ny rute i databasen.
+        // Rute eksempelRute = new Rute(2,1, 60, 120, 40, 80, 3);
+        // reiseKlar.createFavoriteRoute(eksempelRute);
     }
 }
 
