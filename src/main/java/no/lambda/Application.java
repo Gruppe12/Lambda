019cdd8f31f3ba25e66dbde8f6bb74bfa4968754
Tadas Vaitkevicius <vtadasv12@gmail.com>
2025-11-03 08:46:04 +0100

    Innitail autocomplete, hopper det kommer noe opp som vi finner ut må endres for det føler ikke helt riktig

--- src/main/java/no/lambda/Application.java
+++ src/main/java/no/lambda/Application.java
@@ -234,6 +234,31 @@ public class Application {
       
       
         /*
+         eksempel url: http://localhost:8080/api/trips?typedIn=Osl
+
+         rekommenderer å bygge en delay på 0.200sekunder eller noe på frontend så det føles mer smooth ut mellom inputs.
+        */
+
+        app.get("/api/autocomplete", ctx -> {
+            // Har lagt til denne så de kan ikke spamme servern alt for raskt.
+            NaiveRateLimit.requestPerTimeUnit(ctx, 5, TimeUnit.SECONDS);
+
+            String typedInForAutocomplete = ctx.queryParamAsClass("typedIn", String.class)
+                    .check( inputTypedIn -> !inputTypedIn.isBlank(), "Dette felte kan ikke vare blank!")
+                    .check(inputTypedIn -> inputTypedIn.length() <= 60, "Allt for lang input")
+                    .check(inputTypedIn -> inputTypedIn.matches(allowedCharacters), "Ugyldige tegn")
+                    .get();
+
+            // enTur API som gjør autocomplete
+            // Hopper å få den til å gi bare navn.
+            var fromFeatures = _controller.geoHits(typedInForAutocomplete);
+
+
+            // Sender array som string
+            ctx.json(fromFeatures);
+        });
+
+        /*
          Query Parameter
          eksempel url: http://localhost:8080/api/trips?from=Oslo&to=Bergen&time=2025-10-23T19:37:25.123%2B02:00&arriveBy=false
          ADVARSEL!! --- Tiden må være i formatet 2025-10-23T19:37:25.123%2B02:00 og kan ikke være tilbake i tid.
