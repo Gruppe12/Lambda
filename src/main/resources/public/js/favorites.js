2fcdae089c9c2ebb4bdf2ebfa38adca68cbbedd5
churchred <churchred@outlook.com>
2025-11-06 15:56:44 +0100

    Update favorites.js

--- src/main/resources/public/js/favorites.js
+++ src/main/resources/public/js/favorites.js
@@ -4,6 +4,6 @@
 async function getFavoritesFromAPI(){
 
 
-    const favorits = await fetch('http://localhost:8080/api/getFavorits', {headers: {"Bruker-id": "123"}});
+    const favorits = await fetch('http://localhost:8080/api/getFavorits', {headers: {"Bruker-id": "1"}});
     console.log(favorits)
 }
