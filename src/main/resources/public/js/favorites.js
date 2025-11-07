ef761bab4b1c56b3766ec0c2df083d2085bf6e70
churchred <churchred@outlook.com>
2025-11-07 10:53:45 +0100

    Update favorites.js

--- src/main/resources/public/js/favorites.js
+++ src/main/resources/public/js/favorites.js
@@ -4,11 +4,8 @@
 async function getFavoritesFromAPI(){
 
     // Henter API data
-    const response = await fetch('http://localhost:8080/api/getFavorites', {headers: {"Bruker-id": "1"}});
-
-    // Gjør dataen om til noe lesbart
-    const data = await response.json()
+    const response = await fetch('http://localhost:8080/api/getFavorites', {headers: {"Bruker-id": 1}});
 
     // Printer data
-    console.log("API trip data", data)
+    console.log("API trip data", response)
 }
