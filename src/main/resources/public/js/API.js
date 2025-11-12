c037fa89854306560699809aae803f966d374e1d
churchred <98105070+churchred@users.noreply.github.com>
2025-11-12 17:26:06 +0100

    Massive frontend update
    -Flyttet alle API-kall til en egen fil for å gjøre det enklere å oppdatere URL-er som brukes flere steder på siden vår.
    
    -La til et API som sjekker om en rute er en favoritt eller ikke.
    
    -La til logikk som styrer om "legg til rute som favoritt" eller "dette er en favoritt" vises basert på data fra API-et.
    
    -La til bedre utseende for ruter når de er lukket.
    
    -La til brukernavn oppe i høyre hjørne.
    
    -La til favicon for nettsiden.

---
+++ src/main/resources/public/js/API.js
@@ -0,0 +1,57 @@
+
+
+
+// Removes favorite based on fav-id
+async function removeFavoriteAPI(favId){
+
+    const response = await fetch(`http://localhost:8080/api/removeFavorite?favoritId=${favId}`, { headers: { "Bruker-id": getUserId() } })
+
+    return response
+}
+
+
+// Gets all favorites from a user from the API
+// Then sends the data to a func that builds it on the page.
+async function getFavoritesFromAPI(){
+    
+    const response = await fetch('http://localhost:8080/api/getFavorites', {headers: {"Bruker-id": getUserId()}});
+
+    return response;
+}
+
+
+// Sjekker om denne ruten finnes i favorittlisten eller ikke
+async function checkIfFavoriteAPI(fromLat, fromLon, toLat, toLon){
+
+    // Legger kordinatene i riktig format
+    const fromCoords = `${fromLat},${fromLon}`;
+    const toCoords = `${toLat},${toLon}`;
+
+    const response = await fetch(`http://localhost:8080/api/checkIfFavorite?fromCoords=${fromCoords}&toCoords=${toCoords}`, { headers: { "Bruker-id": getUserId() } })
+
+    return response
+}
+
+
+// Legger til et nytt element i databasen
+async function addToFavoriteAPI(fromLat, fromLon, toLat, toLon){
+
+    // Legger kordinatene i riktig format
+    const fromCoords = `${fromLat},${fromLon}`;
+    const toCoords = `${toLat},${toLon}`;
+    
+    const response = await fetch(`/api/addToFavorite?fromCoords=${fromCoords}&toCoords=${toCoords}`, {headers: {"Bruker-id": getUserId()}});
+
+    return response;
+
+}
+
+
+// Lager en rute via EnTur sin API
+async function makeTripAPI(from, to, time) {
+
+    const response = await fetch(`/api/trips?from=${from}&to=${to}&time=${time}%2B02:00&arriveBy=false`)
+
+    return response;
+}
+
